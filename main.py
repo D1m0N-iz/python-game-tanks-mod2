ec82e591e5fd1dd23a0e89c6a7ef966b8ee24602
D1m0N-iz <d.a.izvekov@mail.ru>
2026-08-01 19:55:58 +0300

    Написал методы танка

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from tank import tank
+w.tittle('танчики 2.0')
 
-t1 = tank(x=0,y=0)
 
-t2 = tank(x=100,y=200,model='tiger',ammo=25)
 
-print (tank.count)
+# t1 = tank(x=0,y=0)
+
+# t2 = tank(x=100,y=200,model='tiger',ammo=25,)
+
+# t2.forward()
+# t2.forward()
+# t2.right()
+# t2.fire()
+
+
+# print (t1.ammo)
+# t1.fire()
+# print (t1.ammo)
+
+# print (tank.count)
 
 # for t in[t1,t2]:
 #     print(t.model)
+
--- tank.py
+++ tank.py
@@ -1,9 +1,9 @@
 class tank:
     count = 0
-    def __init__(self,x,y,fuel,hp,ammo = 30,model = 'ис 2'):
+    def __init__(self,x,y,ammo = 30,model = 'ис 2'):
         tank.count += 1
-        self.model = self.model
-        self.fuel = 100
+        self.model = model
+        self.fuel = 1000
         self.hp = 100
         self.xp = 0
         self.ammo = ammo
@@ -16,5 +16,32 @@ class tank:
         if self.y < 0:
             self.y = 0
 
+    def __str__(self):
+        return f'x={self.x},y={self.y},ammo={self.ammo}'
 
-        def fire(self):
+    def show_info(self):
+        print(f'x={self.x},y={self.y},ammo={self.ammo}',)
+
+    def fire(self):
+        if self.ammo > 0:
+            self.ammo -= 1
+            print('выстрел!!!')
+    def forward(self):
+        if self.fuel > 0:
+            self.y -= 1
+            self.fuel -=1
+
+    def backward(self):
+        if self.fuel > 0:
+            self.y += 1
+            self.fuel -= 1
+
+    def left(self):
+        if self.fuel > 0:
+         self.x -= 1
+         self.fuel -=1
+                
+    def right(self):
+        if self.fuel > 0:
+            self.x += 1
+            self.fuel -= 1
