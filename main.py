6b9dccbab393728634129a9b3b55796ac9e01a8d
D1m0N-iz <d.a.izvekov@mail.ru>
2026-08-02 14:57:09 +0300

    Исправил отступы

--- main.py
+++ main.py
@@ -10,21 +10,23 @@ def key_press(event):
     elif event.keycode == KEY_A:
         player.left()
     elif event.keycode == KEY_S:
-            player.backward()
+        player.backward()
     elif event.keycode == KEY_D:
-                player.right()
+        player.right()
+    print(player)
 
-w = tk.Tk()
-w.title('танчики 2.0')
-canvas = tk.Canvas(w,width=800,height=600, bg='#6a6a6a')
-canvas.pack()
+w = tk.Tk() # Создаем объект окна
+w.title('Танки на минималках 2.0') # Заголовок окна
+canvas = tk.Canvas(w, width=800, height=600, bg="#6A6A6A") # Создаем холст в окне
+canvas.pack() # Располагаем холст в окне
 
-player = tank(canvas=canvas,x=100,y=50)
-anemy = tank(canvas=canvas,x=500,y=150)
+player = tank(canvas=canvas, x=100, y=50) # Танк игрока
+enemy = tank(canvas=canvas, x=500, y=150) # Танк врага
 
 w.bind('<KeyPress>', key_press)
 
 w.mainloop()
+
 # t1 = tank(x=0,y=0)
 
 # t2 = tank(x=100,y=200,model='tiger',ammo=25,)
--- tank.py
+++ tank.py
@@ -2,11 +2,11 @@ class tank:
     count = 0
     SIZE = 100
 
-    def __init__(self,canvas,x,y,ammo = 30,model = 'ис 2',speed=10):
+    def __init__(self, canvas, x, y, model='ис 2', ammo=30, speed=10):
         tank.count += 1
         self.canvas = canvas
         self.model = model
-        self.fuel = 1000
+        self.fuel = 1000 # Запас топлива
         self.hp = 100
         self.xp = 0
         self.ammo = ammo
@@ -22,14 +22,13 @@ class tank:
 
         self.create()
 
-
     def __str__(self):
-        return f'x={self.x},y={self.y},ammo={self.ammo}'
+        return f'x={self.x} y={self.y} ammo={self.ammo}'
 
-    def create(self):
-        self.id = self.canvas.create_rectangle(self.x,self.y,self.x + tank.SIZE,self.y + tank.SIZE)
+    def create(self): # Метод отрисовки квадрата танка
+        self.id = self.canvas.create_rectangle(self.x, self.y, self.x + tank.SIZE, self.y + tank.SIZE)
 
-    def repaint(self):
+    def repaint(self): # Метод перерисовки танка
         self.canvas.moveto(self.id, x=self.x, y=self.y)
 
     def show_info(self):
@@ -42,24 +41,24 @@ class tank:
 
     def forward(self):
         if self.fuel > 0:
-            self.y -= 1
-            self.fuel -=1
+            self.y -= self.speed
+            self.fuel -= 1
             self.repaint()
 
     def backward(self):
         if self.fuel > 0:
-            self.y += 1
+            self.y += self.speed
             self.fuel -= 1
             self.repaint()
 
     def left(self):
         if self.fuel > 0:
-           self.x -= 1
-           self.fuel -=1
-           self.repaint()
-                
+            self.x -= self.speed
+            self.fuel -= 1
+            self.repaint()
+
     def right(self):
         if self.fuel > 0:
-            self.x += 1
+            self.x += self.speed
             self.fuel -= 1
             self.repaint()
