5a7af7d05597cc6ee4e6777b4401db91e308c08c
D1m0N-iz <d.a.izvekov@mail.ru>
2026-08-02 14:28:40 +0300

    Начал вырисовывать танк

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+import tkinter as tk
 from tank import tank
-w.tittle('танчики 2.0')
 
+# Коды клавиш
+KEY_W, KEY_A, KEY_S, KEY_D = 87, 65, 83, 68
 
+def key_press(event):
+    if event.keycode == KEY_W:
+        player.forward()
+    elif event.keycode == KEY_A:
+        player.left()
+    elif event.keycode == KEY_S:
+            player.backward()
+    elif event.keycode == KEY_D:
+                player.right()
 
+w = tk.Tk()
+w.title('танчики 2.0')
+canvas = tk.Canvas(w,width=800,height=600, bg='#6a6a6a')
+canvas.pack()
+
+player = tank(canvas=canvas,x=100,y=50)
+anemy = tank(canvas=canvas,x=500,y=150)
+
+w.bind('<KeyPress>', key_press)
+
+w.mainloop()
 # t1 = tank(x=0,y=0)
 
 # t2 = tank(x=100,y=200,model='tiger',ammo=25,)
--- tank.py
+++ tank.py
@@ -1,12 +1,16 @@
 class tank:
     count = 0
-    def __init__(self,x,y,ammo = 30,model = 'ис 2'):
+    SIZE = 100
+
+    def __init__(self,canvas,x,y,ammo = 30,model = 'ис 2',speed=10):
         tank.count += 1
+        self.canvas = canvas
         self.model = model
         self.fuel = 1000
         self.hp = 100
         self.xp = 0
         self.ammo = ammo
+        self.speed = speed
 
         self.x = x
         self.y = y
@@ -16,8 +20,17 @@ class tank:
         if self.y < 0:
             self.y = 0
 
+        self.create()
+
+
     def __str__(self):
         return f'x={self.x},y={self.y},ammo={self.ammo}'
+
+    def create(self):
+        self.id = self.canvas.create_rectangle(self.x,self.y,self.x + tank.SIZE,self.y + tank.SIZE)
+
+    def repaint(self):
+        self.canvas.moveto(self.id, x=self.x, y=self.y)
 
     def show_info(self):
         print(f'x={self.x},y={self.y},ammo={self.ammo}',)
@@ -26,22 +39,27 @@ class tank:
         if self.ammo > 0:
             self.ammo -= 1
             print('выстрел!!!')
+
     def forward(self):
         if self.fuel > 0:
             self.y -= 1
             self.fuel -=1
+            self.repaint()
 
     def backward(self):
         if self.fuel > 0:
             self.y += 1
             self.fuel -= 1
+            self.repaint()
 
     def left(self):
         if self.fuel > 0:
-         self.x -= 1
-         self.fuel -=1
+           self.x -= 1
+           self.fuel -=1
+           self.repaint()
                 
     def right(self):
         if self.fuel > 0:
             self.x += 1
             self.fuel -= 1
+            self.repaint()
